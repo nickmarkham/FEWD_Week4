9788e8f5a20d3537073884efc377d7115334d584
Nick Markham <nickmarkham@gmail.com>
2016-01-21 17:21:46 +0800

    Completed JQuery exercises and Assignment
    getting there piece by piece

--- jquery_traffic_light_exercise/js/index.js
+++ jquery_traffic_light_exercise/js/index.js
@@ -1,1 +1,27 @@
-//Implement the red light using jQuery. Don't forget to add the script tags.
+
+$("#stopButton").click(illuminateRed);
+$("#slowButton").click(illuminateYellow);
+$("#goButton").click(illuminateGreen);
+
+function illuminateRed() {
+  clearLights();
+$("#stopLight").css("background-color", "red")
+}
+
+function illuminateYellow() {
+  clearLights();
+$("#slowLight").css("background-color", "yellow")
+}
+
+function illuminateGreen() {
+  clearLights();
+$("#goLight").css("background-color", "green")
+}
+
+function clearLights() {
+
+$("#stopLight").css("background-color", "black");
+$("#slowLight").css("background-color", "black");
+$("#goLight").css("background-color", "black");
+
+}
